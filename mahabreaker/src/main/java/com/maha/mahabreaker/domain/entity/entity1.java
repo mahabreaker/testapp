1440f482b1b2e5e7cdde26c42203ef106c239aec
csshin <mahabreaker@gmail.com>
2022-01-23 19:11:00 +0900

    commit-subject-01
    commit-subject-02

--- mahabreaker/src/main/java/com/maha/mahabreaker/domain/entity/entity1.java
+++ mahabreaker/src/main/java/com/maha/mahabreaker/domain/entity/entity1.java
@@ -1,5 +1,9 @@
 package com.maha.mahabreaker.domain.entity;
 
+import javax.persistence.Entity;
+
+@Entity
 public class entity1 {
     private String companyAddress;
+    private String companyName;
 }
--- mahabreaker/src/main/java/com/maha/mahabreaker/domain/entity/entity2.java
+++ mahabreaker/src/main/java/com/maha/mahabreaker/domain/entity/entity2.java
@@ -1,5 +1,9 @@
 package com.maha.mahabreaker.domain.entity;
 
+import javax.persistence.Entity;
+
+@Entity
 public class entity2 {
-    
+    private long user_id;
+    private long user_age;
 }
